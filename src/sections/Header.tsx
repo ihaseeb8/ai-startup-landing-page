1d676652f42dc5ee18988623ae9a811c64c76ef3
Haseeb Ullah <ihaseebullah.8@gmail.com>
2024-08-18 15:09:29 +0500

    Added Features Section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -6,8 +6,9 @@ export const Header = () => {
   return  (
 
     <header className="py-4 border-b border-white/15 md:border-none sticky top-0 z-10">
+      <div className='absolute inset-0 backdrop-blur -z-10 md:hidden'></div>
       <div className="container">
-        <div className="flex justify-between items-center md:border border-white/15 backdrop-blur md:p-2.5 rounded-xl max-w-2xl mx-auto">
+        <div className="flex justify-between items-center md:border border-white/15 md:backdrop-blur md:p-2.5 rounded-xl max-w-2xl mx-auto">
           <div className='border size-10 inline-flex items-center justify-center rounded-md border-white/15'>
             <LogoIcon className='size-8'/>
           </div>
