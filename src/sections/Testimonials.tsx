44fee546d224ff326be5743f19440ba22519c4d8
Haseeb Ullah <ihaseebullah.8@gmail.com>
2024-08-20 12:12:35 +0500

    Added Animations of Testimonial Section

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,8 +1,12 @@
+"use client";
+
 import avatar1 from "@/assets/avatar-1.png";
 import avatar2 from "@/assets/avatar-2.png";
 import avatar3 from "@/assets/avatar-3.png";
 import avatar4 from "@/assets/avatar-4.png";
 import Image from "next/image";
+
+import { motion } from "framer-motion";
 
 const testimonials = [
   {
@@ -38,30 +42,42 @@ export const Testimonials = () => {
       <h2 className="text-5xl md:text-6xl text-center tracking-tighter font-medium">Beyond Expectations</h2>
       <p className="text-white/70 text-lg md:text-xl text-center mt-5 tracking-tight max-w-sm mx-auto">Our revolutionary AI SEO tools have transformed our clients' strategies.</p>
       
-      <div className="flex gap-5 overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-        
-        {testimonials.map( testimonial => (
-          <div 
-            key={testimonial.name} 
-            className="border border-white/15 p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs md:max-w-md flex-none"
-          >
-            <div className="text-lg md:text-2xl tracking-tight">{testimonial.text}</div>
-            <div className="flex items-center gap-3 mt-5">
-              <div className="relative after:content-[''] after:absolute after:inset-0 after:bg-[rgb(140,69,244)] after:mix-blend-soft-light before:content-[''] before:absolute before:inset-0 before:border before:border-white/30 before:z-10 before:rounded-lg">
-                <Image 
-                  src={testimonial.avatarImg} 
-                  alt={`Avatar for ${testimonial.name}`}
-                  className="size-11 grayscale rounded-lg"
-                  />
+      <div className="flex overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+        <motion.div
+          initial={{
+            translateX: '-50%'
+          }}
+          animate={{
+            translateX: "0",
+          }}
+          transition={{
+            repeat: Infinity,
+            ease: "linear",
+            duration: 30,
+          }}
+          className="flex gap-5 flex-none pr-5">
+            {[...testimonials, ...testimonials].map( (testimonial) => (
+              <div 
+                key={testimonial.name} 
+                className="border border-white/15 p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs md:max-w-md flex-none"
+              >
+                <div className="text-lg md:text-2xl tracking-tight">{testimonial.text}</div>
+                <div className="flex items-center gap-3 mt-5">
+                  <div className="relative after:content-[''] after:absolute after:inset-0 after:bg-[rgb(140,69,244)] after:mix-blend-soft-light before:content-[''] before:absolute before:inset-0 before:border before:border-white/30 before:z-10 before:rounded-lg">
+                    <Image 
+                      src={testimonial.avatarImg} 
+                      alt={`Avatar for ${testimonial.name}`}
+                      className="size-11 grayscale rounded-lg"
+                      />
+                  </div>
+                  <div className="flex flex-col">
+                    <div>{testimonial.name}</div>
+                    <div className="text-white/50 text-sm">{testimonial.title}</div>
+                  </div>
+                </div>
               </div>
-              <div className="flex flex-col">
-                <div>{testimonial.name}</div>
-                <div className="text-white/50 text-sm">{testimonial.title}</div>
-              </div>
-            </div>
-          </div>
-        ))}
-      
+          ))}
+        </motion.div>
       </div>
     </div>
   </section>;
