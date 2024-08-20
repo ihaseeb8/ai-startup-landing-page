3c23c9239ff10fadf6df8fa187b0343b50fd0dcf
Haseeb Ullah <ihaseebullah.8@gmail.com>
2024-08-20 13:20:30 +0500

    Added final animations and minor fixes

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,17 +1,82 @@
+'use client'
+
 import { Button } from "@/components/Button";
 import starsBg from '@/assets/stars.png'
 import gridLines from '@/assets/grid-lines.png'
 
+import { RefObject, useEffect, useRef } from "react";
+import { motion, useMotionTemplate, useMotionValue, useScroll, useTransform } from "framer-motion";
+
+const useRelativeMousePosition = (to: RefObject<HTMLElement>) => {
+  const mouseX = useMotionValue(0)
+  const mouseY = useMotionValue(0)
+
+  const updateMousePosition = (event: MouseEvent) => {
+    if(!to.current) return
+
+    const { top , left} = to.current.getBoundingClientRect()
+
+    mouseX.set(event.x - left);
+    mouseY.set(event.y - top);
+  }
+
+  useEffect(()=>{
+    window.addEventListener('mousemove', updateMousePosition);
+
+    return () =>{
+      window.addEventListener('mousemove', updateMousePosition);
+    }
+  }, [])
+  return [mouseX, mouseY]
+}
+
+
 export const CallToAction = () => {
+
+  const sectionRef = useRef<HTMLElement>(null);
+  const borderedDivRef = useRef<HTMLDivElement>(null);
+
+  const {scrollYProgress} = useScroll({
+    target: sectionRef,
+    offset: ['start end', 'end start'] 
+  })
+
+  const backgroundPositionY = useTransform(scrollYProgress, [0, 1], [-300, 300])
+  const [mouseX, mouseY] = useRelativeMousePosition(borderedDivRef)
+
+  const maskImage= useMotionTemplate`radial-gradient(50% 50% at ${mouseX}px ${mouseY}px,black,transparent)`
+
+  
   return (
-  <section className="py-20 md:py-24">
+  <section
+  
+  ref={sectionRef}
+  className="py-20 md:py-24">
     <div className="container">
-      <div className="border border-white/15 py-24 rounded-xl overflow-hidden relative" style={{
-        backgroundImage: `url(${starsBg.src})`
+      <motion.div 
+        ref={borderedDivRef}
+        className="border border-white/15 py-24 rounded-xl overflow-hidden relative group" 
+        animate={{
+          backgroundPositionX: starsBg.width,
+        }}
+        transition={{
+          repeat: Infinity,
+          duration: 60,
+          ease: 'linear'
+        }}
+        style={{
+          backgroundPositionY,
+          backgroundImage: `url(${starsBg.src})`
       }}>
-        <div className="absolute bg-purple-800 inset-0 bg-blend-overlay [mask-image:radial-gradient(50%_50%_at_50%_35%,black,transparent)]" style={{
+        <div className="absolute bg-purple-800 inset-0 bg-blend-overlay [mask-image:radial-gradient(50%_50%_at_50%_35%,black,transparent)] group-hover:opacity-0 transition duration-700" style={{
           backgroundImage: `url(${gridLines.src})`
         }}></div>
+
+        <motion.div className="absolute bg-purple-800 inset-0 bg-blend-overlay opacity-0 group-hover:opacity-100 transition duration-700" 
+        style={{
+          maskImage,
+          backgroundImage: `url(${gridLines.src})`
+        }}></motion.div>
 
         <div className="relative">
           <h2 className="text-5xl md:text-6xl max-w-sm mx-auto tracking-tighter text-center font-medium">AI-Driven SEO for everyone.</h2>
@@ -21,7 +86,7 @@ export const CallToAction = () => {
           </div>
         
         </div>
-      </div>
+      </motion.div>
     </div>
   </section>
   );
--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -40,7 +40,7 @@ export const Testimonials = () => {
     <div className="container">
       
       <h2 className="text-5xl md:text-6xl text-center tracking-tighter font-medium">Beyond Expectations</h2>
-      <p className="text-white/70 text-lg md:text-xl text-center mt-5 tracking-tight max-w-sm mx-auto">Our revolutionary AI SEO tools have transformed our clients' strategies.</p>
+      <p className="text-white/70 text-lg md:text-xl text-center mt-5 tracking-tight max-w-sm mx-auto">Our revolutionary AI SEO tools have transformed our clients strategies.</p>
       
       <div className="flex overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
         <motion.div
